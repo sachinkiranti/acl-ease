d5925c373a0926f54b0ceb71aef27e729e7849ba
sachinkiranti <sachinkiranti@gmail.com>
2023-07-20 20:59:42 +0545

    feat: add required models

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -2,7 +2,29 @@
 
 namespace SachinKiranti\AclEase\Models;
 
-class Permission
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Permission extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'description', 'status'
+    ];
+
+    /**
+     * Permission belongs to many roles
+     *
+     * @return BelongsToMany
+     */
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany( config('acl-ease.models.role', Role::class) );
+    }
 
 }
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,7 +2,75 @@
 
 namespace SachinKiranti\AclEase\Models;
 
-class Role
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'description', 'status', 'parent_id',
+    ];
+
+    /**
+     * Roles belongs to many permissions
+     *
+     * @return BelongsToMany
+     */
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany( config('acl-ease.models.permission', Permission::class) )->withTimestamps();
+    }
+
+    /**
+     * Roles belongs to many users
+     *
+     * @return BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany( config('acl-ease.models.user', User::class) );
+    }
+
+    /**
+     * @param string $permission
+     * @return bool
+     */
+    public function inRole(string $permission): bool
+    {
+        if ($permission) {
+            return $this->permissions->contains('slug', $permission);
+        }
+        return !! $permission->intersect($this->permissions)->count();
+    }
+
+    /**
+     * Check whether the role contains the provided permission or not
+     *
+     * @param int $permission
+     * @return boolean
+     *
+     */
+    public function containsPermission($permission): bool
+    {
+        return $this->permissions->contains($permission);
+    }
+
+    /**
+     * Assign the permissions to given role
+     *
+     * @param array $permissions
+     * @return mixed
+     */
+    public function assignPermission(array $permissions): mixed
+    {
+        return $this->permissions()->sync($permissions);
+    }
 
 }
